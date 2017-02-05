26af240b1805458ae0dae50700253dce4bfa60a5
Rogerio Prado de Jesus <rogeriopradoj@gmail.com>
2017-02-04 22:26:18 -0200

    Improve scripts/list-asc-files-non-utf8-or-ascii.sh

--- scripts/list-asc-files-non-utf8-or-ascii.sh
+++ scripts/list-asc-files-non-utf8-or-ascii.sh
@@ -1,9 +1,7 @@
 #!/usr/bin/env bash
 to_utf8_recurse() {
     for file in "$1"/*;do
-        #echo $file; exit;
         if [ -d "$file" ];then
-            printf ">dir: $file \n=====\n"
             to_utf8_recurse "$file";
 
         elif [ -f "$file" ];then
@@ -12,21 +10,13 @@ to_utf8_recurse() {
 
                 if ! ([[ $ENCODING == "utf-8" ]] || [[ $ENCODING == "us-ascii" ]]);then
                     RESULT=$(file --mime-encoding $file)
-                    printf "        > File no utf-8 or us-ascii > $RESULT\n"
-                    #printf "        > Before > $RESULT\n"
-
-                    # iconv -f $ENCODING -t utf-8 "$file" > "$file.1"
-                    # mv "$file.1" "$file"
-
-                    # RESULT=$(file --mime-encoding $file)
-                    # printf "        > After > $RESULT\n"                    
-
-
+                    printf "  > $RESULT\n"
                 fi
             fi
         fi
     done
 }
 
+printf "$1\n=====\n";
 to_utf8_recurse $1
 exit;
