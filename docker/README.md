120f8aa1c543f3d14ad9bc4ff94f30b99360226e
Rogerio Prado de Jesus <rogeriopradoj@gmail.com>
2017-02-06 12:15:11 -0200

    Improve docker image

--- docker/README.md
+++ docker/README.md
@@ -2,7 +2,21 @@
 
 Docker Image to generate books (epub, mobi and pdf) from source of [progit2 repositories](https://github.com/progit/progit2).
 
-It's appropriated either for English official repository as for the translations that are done via forks from the English one.
+It's appropriated either for English official repository as for the translations made via forks from the English official one.
+
+## How to Use
+
+```
+$ cd PATH_PROGIT2_REPOSITORY
+
+$ docker run -it --rm \
+    -v `pwd`:/documents/ \
+    rogeriopradoj/progit2
+```
+
+## Dockerfile
+
+<https://github.com/progit2-pt-br/progit2/tree/master/docker>
 
 ## Maintainers
 
--- docker/scripts/start-container.sh
+++ docker/scripts/start-container.sh
@@ -1,2 +1,21 @@
 #!/bin/sh
-bundle exec rake book:build
+[ -d images ] || mkdir images
+for image in book/*/images/*; do
+    cp "$image" "images/$(basename "$image")"
+done
+
+printf "Converting to HTML...\n"
+asciidoctor progit.asc
+printf " -- HTML output at progit.html\n"
+
+printf "Converting to EPub...\n"
+asciidoctor-epub3 progit.asc
+printf " -- Epub output at progit.epub\n"
+
+printf "Converting to Mobi (kf8)...\n"
+asciidoctor-epub3 -a ebook-format=kf8 progit.asc
+printf " -- Mobi output at progit.mobi\n"
+
+printf "Converting to PDF... (this one takes a while)\n"
+asciidoctor-pdf progit.asc 2>/dev/null
+printf " -- PDF  output at progit.pdf\n"
